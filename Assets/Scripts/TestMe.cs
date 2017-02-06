6cf1c097f2a31af671fa17d2a3f699f5192a8740
Matthew Williamson <chuwilliamson@gmail.com>
2017-02-05 19:21:23 -0600

    initial commit

---
+++ Assets/Scripts/TestMe.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Cos(Time.realtimeSinceStartup), 0, 0);
+	}
+}
+
